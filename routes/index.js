5991ecfd6f0a3b555e10ae7c80d414d8e30856cd
2214027 <2214027@slu.edu.ph>
2023-12-01 17:48:25 +0800

    fixed

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,54 @@
-  var express = require('express');
-  var router = express.Router();
+var express = require('express');
+var router = express.Router();
+var database = require('../database');
 
-  var database = require('../database');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', session: req.session });
+});
 
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', session : req.session });
-  });
+router.post('/', function(req, res, next) {
+  var userEmailAddress = req.body.user_email_address;
+  var userPassword = req.body.user_password;
 
-  router.post('/', function(req, res, next){
-    var userEmailAddress = request.body.user_email_address;
-    var userPassword = request.body.user_password;
+res.redirect('/test');
+  // if (userEmailAddress && userPassword) {
+  //   // Assuming you want to retrieve user data based on the email address
+  //   var query = `
+  //     SELECT * FROM user
+  //     WHERE email = "${userEmailAddress}"
+  //   `;
 
-    if(userEmailAddress && userPassword)
-    {
-        query = `
-        SELECT * FROM user
-        WHERE email = "${userEmailAddress}"
-        `;
+  //   database.query(query, function(error, data) {
+  //     if (error) {
+  //       // Handle the database query error
+  //       console.error(error);
+  //       return res.status(500).send('Internal Server Error');
+  //     }
 
-        database.query(query, function(error, data){
-            if(data.length > 0)
-            {
-                for(var count = 0; count < data.length; count++)
-                {
-                    if(data[count].user_password == userPassword)
-                    {
-                        request.session.userid = data[count].userid;
+  //     if (data.length > 0) {
+  //       // Assuming only one user with the provided email address is found
+  //       var user = data[0];
 
-                        response.redirect("/views/test.html");
-                    }
-                    else
-                    {
-                        response.send('Incorrect Password');
-                    }
-                }
-            }
-            else
-            {
-                response.send('Incorrect Email Address');
-            }
-            response.end();
-        });
-    }
-    else
-    {
-        
-    }
-  })
+  //       if (user.user_password === userPassword) {
+  //         req.session.userid = user.userid;
+  //         return res.redirect('/test');
+  //       } else {
+  //         return res.send('Incorrect Password');
+  //       }
+  //     } else {
+  //       return res.send('Incorrect Email Address');
+  //     }
+  //   });
+  // } else {
+  //   // Handle the case where either email or password is missing
+  //   return res.send('Email and password are required');
+  // }
+});
 
-  router.get('/logout', function(request, response, next){
-    request.session.destroy();
-    response.redirect("/");
-  })
-  module.exports = router;
+router.get('/logout', function(request, response, next) {
+  request.session.destroy();
+  response.redirect("/");
+});
+
+module.exports = router;

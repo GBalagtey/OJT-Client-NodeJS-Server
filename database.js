3b1230ce68231b7d3e870e9104aac803e7c42c90
Kaizer <2222613@slu.edu.ph>
2023-12-02 10:23:42 +0800

    added functionalities for the login page

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    database: 'webdev',
+    database: 'ojt_portaldb',
     user: 'root',
     password: ''
 });
---
+++ insertUser.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const mysqlConnection = require('./database'); // Update with the correct path
+const saltRounds = 10;
+
+async function hashPassword(password) {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw error;
+  }
+}
+
+class User {
+    static async insertUser(email, password, userType) {
+      try {
+        const hashedPassword = await hashPassword(password);
+  
+        // Log the generated hashed password
+        console.log('Generated Hashed Password (insertUser):', hashedPassword);
+  
+        // SQL query to insert user into the users table
+        const query = 'INSERT INTO users (email, hashedPassword, userType) VALUES (?, ?, ?)';
+        const values = [email, hashedPassword, userType];
+  
+        // Execute the query
+        mysqlConnection.query(query, values, (error, results) => {
+          if (error) {
+            throw error;
+          }
+  
+          console.log('User inserted successfully:', results.insertId);
+        });
+      } catch (error) {
+        console.error('Error inserting user:', error.message);
+      }
+    }
+  }
+  
+
+// Example usage
+const userEmail = '2222613@slu.edu.ph';
+const userPassword = 'samplepass';
+const userType = 'student';
+
+// Call the insertUser method
+User.insertUser(userEmail, userPassword, userType);

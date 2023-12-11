0eb9974f06ec54a71f196a786a83647691861196
gregg <2214027@slu.edu.ph>
2023-12-12 00:35:23 +0800

    fixed announcement and ojtrecordhistory for faculty

--- public/javascripts/dashboardFaculty.js
+++ public/javascripts/dashboardFaculty.js
@@ -34,6 +34,41 @@ function populateStudentRecords() {
     });
 }
 
+function populateAnnouncement() {
+  fetch('/getAnnouncements')
+    .then(response => response.json())
+    .then(records => {
+      if (records.length === 0) {
+        updates.innerHTML = '<p>No announcements available</p>';
+      } else {
+        records.forEach(record => {
+          const newDiv = document.createElement("div");
+          newDiv.classList.add("announcement");
+
+          const titleElement = document.createElement("h3");
+          titleElement.textContent = record.title;
+
+          const dateElement = document.createElement("small");
+          dateElement.textContent = `Date: ${formatDate(record.date)}`; // Use formatDate function here
+
+          const textElement = document.createElement("p");
+          textElement.textContent = record.text;
+
+          newDiv.appendChild(titleElement);
+          newDiv.appendChild(dateElement);
+          newDiv.appendChild(textElement);
+
+          updates.appendChild(newDiv);
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching announcements:', error);
+      updates.innerHTML = '<p>Error fetching announcements</p>';
+    });
+}
+
+
 function populate(records){
   recordsTable.innerHTML = '';
   records.forEach(record => {
@@ -227,6 +262,7 @@ function parseTime(timeString) {
   const [hours, minutes, seconds] = timeString.split(':').map(Number);
   return hours * 3600 + minutes * 60 + seconds;
 }
+
 
 populateStudentRecords();
 populateStudentRecords2();

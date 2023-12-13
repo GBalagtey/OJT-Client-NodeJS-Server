af2bcb46b5730406d68e6225834eb266d5d74dfd
Kaizer <2222613@slu.edu.ph>
2023-12-14 00:31:04 +0800

    further progress on display

--- public/javascripts/studentTable.js
+++ public/javascripts/studentTable.js
@@ -14,10 +14,10 @@ function populateStudentRecords() {
                 const row = document.createElement('tr');
                 row.innerHTML = `
                     <td>${record.firstName} ${record.lastName}</td>
-                    <td>${record.companyName}</td>
-                    <td>${record.studEmail}</td>
+                    <td style="${record.companyName !== null ? '' : 'color: rgb(165, 42, 42);'}">${record.companyName !== null ? record.companyName : 'None'}</td>
+                    <td>${record.email}</td>
                     <td>${record.teacherName}</td>
-                    <td>${record.supervisor}</td>
+                    <td style="${record.supervisor !== null ? '' : 'color: rgb(165, 42, 42);'}">${record.supervisor !== null ? record.supervisor : 'None'}</td>
                 `;
   
                 recordsTable.appendChild(row);

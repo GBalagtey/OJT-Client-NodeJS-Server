5b235bc58e895e5a2c2019d275080a521cd2dcda
Kaizer <2222613@slu.edu.ph>
2023-12-16 14:44:44 +0800

    added add company details feature for student

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -1,20 +1,25 @@
-const modal = document.getElementById('myModal');
+const modalWork = document.getElementById('myModal');
 const modalBtn = document.getElementById('modalBtn');
-const closeBtn = document.getElementsByClassName('close')[0];
+const closeWork = document.getElementById('closeWorkModal')
+const closeCompany =document.getElementById('closeCompanyModal')
 const workForm = document.getElementById('workForm');
 const currentDate = document.getElementById('currentDate');
+const modalCompany = document.getElementById('companyModal');
 
 modalBtn.onclick = function() {
-    modal.style.display = 'block';
+    modalWork.style.display = 'block';
 }
 
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
+closeWork.onclick = function() {
+    modalWork.style.display = 'none';
+}
+closeCompany.onclick = function(){
+    modalCompany.style.display = 'none';
 }
 
 window.onclick = function(event) {
     if (event.target === modal) {
-        modal.style.display = 'none';
+        modalWork.style.display = 'none';
     }
 }
 
@@ -34,13 +39,7 @@ const formattedDate = interpretedDate.toLocaleDateString('en-US', {
 
 currentDate.innerText = formattedDate + ' (' + interpretedDate.toLocaleDateString('en-US', { weekday: 'short' }) + ')';
 
-
-// workForm.addEventListener('submit', function(e) {
-//     e.preventDefault();
-//     const renderedHours = document.getElementById('renderedHours').value;
-//     const description = document.getElementById('description').value;
-//     // Here, you can handle the submitted data (renderedHours and description)
-//     // For example, you might send it to a server using fetch or perform other actions.
-//     // Then, you can close the modal:
-//     modal.style.display = 'none';
-// });
+function addCompany(){
+    const companyModal = document.getElementById('companyModal');
+    companyModal.style.display = "block";
+}
